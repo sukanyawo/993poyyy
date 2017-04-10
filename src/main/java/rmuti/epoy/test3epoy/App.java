b804b09046e1a7045d4b58ea480e13838ee83e97
POY <993sukunya@gmail.com>
2017-04-10 12:13:18 +0700

    K

---
+++ src/main/java/rmuti/epoy/test3epoy/App.java
@@ -0,0 +1,11 @@
+package rmuti.epoy.test3epoy;
+
+
+public class App 
+{
+    public void addDiv3(Object e)
+    {
+    	Integer i = (Integer) e;
+    	lst.add(e);
+    }
+}
---
+++ src/main/java/rmuti/epoy/test3epoy/ArrayList.java
@@ -0,0 +1,8 @@
+package rmuti.epoy.test3epoy;
+
+public class ArrayList {
+	public ArrayList(){
+	 ArrayList lst=new ArrayList();
+			
+	}
+}
